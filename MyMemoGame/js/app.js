c96e82c2d89c466e9b96150f202982f2864fde0d
Dora-Lencse <dora.lencse@gmail.com>
2018-03-21 21:36:34 +0100

    Feat: ClickOnCards; compareCards
    At the moment not working well, it's needed to be fixed some issue.

--- MyMemoGame/js/app.js
+++ MyMemoGame/js/app.js
@@ -15,6 +15,18 @@ let cards = ['fa fa-diamond',
 			 'fa fa-bicycle', 
 			 'fa fa-paper-plane-o', 
 			 'fa fa-cube'];
+			 
+let moves = 0;
+const cardContent = document.querySelectorAll('li.card');
+const pickCard = document.getElementsByClassName('card');
+const openedCards = document.getElementsByClassName('open show');
+const opened = [];
+
+
+startGame();
+clickOnCards();
+console.log(openedCards);
+console.log(opened);
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -31,11 +43,12 @@ function shuffle(array) {
     return array;
 }
 
+function startGame() {
 cards = shuffle(cards);
 
-const cardContent = document.querySelectorAll('li.card');
 for(let i = 0; i < cardContent.length; i++){
 	cardContent[i].innerHTML = '<i class="'+cards[i]+'"></i>';
+}
 }
 
 /*
@@ -48,3 +61,36 @@ for(let i = 0; i < cardContent.length; i++){
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+ 
+ 
+function clickOnCards() {
+for(let i = 0; i < pickCard.length; i++){
+	pickCard[i].addEventListener('click', function() {
+	pickCard[i].setAttribute('class', 'card open show');
+	opened.push(pickCard[i]);
+	compareCards();
+	moves++;
+});
+}
+}
+
+function compareCards () {
+
+	if (opened.length == 2 ) {		
+			
+			if(opened[0].innerHTML !== opened[1].innerHTML) {
+			opened[0].setAttribute('class', 'card');
+			opened[1].setAttribute('class', 'card');
+			opened.pop();
+			opened.pop();
+			compareCards();
+			
+			} else if (opened[0].innerHTML === opened[1].innerHTML){
+			opened[0].setAttribute('class', 'card match');
+			opened[1].setAttribute('class', 'card match');
+			opened.pop();
+			opened.pop();
+			compareCards();
+			}		 		
+	}		
+}
