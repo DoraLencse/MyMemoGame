cca48f713354cd51b040cf30bdd89008b4ef6034
Dora-Lencse <dora.lencse@gmail.com>
2018-04-01 21:29:14 +0200

    Feat: Timer is now working

--- MyMemoGame/js/app.js
+++ MyMemoGame/js/app.js
@@ -15,7 +15,6 @@ let cards = ['fa fa-diamond',
 			 'fa fa-paper-plane-o', 
 			 'fa fa-cube'];
 	
-let moves = 0;
 let cardContent = document.querySelectorAll('li.card');
 let pickCard = document.getElementsByClassName('card');
 let picked = [];
@@ -25,10 +24,17 @@ let flipBack = document.getElementsByClassName('nomatch');
 let matchedCards = document.getElementsByClassName('match');
 let matched = [];
 let movesCount = document.querySelector('span.moves');
+let moves = 0;
 let starFirst = document.getElementById('first-star');
 let starSecond = document.getElementById('second-star');
 let starThird = document.getElementById('third-star');
 let restartIcon = document.querySelector('div.restart');
+let finalTime = "";
+let timerDisplay = document.getElementsByClassName('timer-place')[0];
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let t;
 
 restartIcon.onclick = function() {
 	
@@ -44,7 +50,7 @@ restartIcon.onclick = function() {
 	 	
 	restartGame()
 }
-			 
+
 startGame();
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -58,7 +64,7 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
+	
     return array;
 }
 
@@ -75,23 +81,23 @@ clickOnCards();
 }
  
 function clickOnCards() {
-	            
+	         
 			for(let i = 0; i < pickCard.length; i++){				
 			//pickCard[i].addEventListener('click', function() {
 			pickCard[i].onclick = function() {
-		    moves ++;
+			pickCard[i].style.pointerEvents="none";				
+		    moves ++;			
 			movesCount.innerHTML = '<span class="moves">'+moves+'</span>';
-			
 			rating();
-			pickCard[i].setAttribute('class', 'card open show');			
+			pickCard[i].setAttribute('class', 'card open show');
+								
 			opened.push(pickCard[i]);
-  
-  
+			
 			compareCards();
 			
 			}/*)*/;	
-			}			
-
+			}
+			startTimer();
 }
 
 function compareCards () {
@@ -100,9 +106,10 @@ function compareCards () {
 				
 			if(opened[0].innerHTML !== opened[1].innerHTML) {
 			opened[0].setAttribute('class', 'card nomatch');
-			opened[1].setAttribute('class', 'card nomatch');		
+			opened[1].setAttribute('class', 'card nomatch');
 			opened.pop();
 			opened.pop();
+			
 			
 			} else if (opened[0].innerHTML === opened[1].innerHTML){
 			opened[0].setAttribute('class', 'card match');
@@ -113,9 +120,13 @@ function compareCards () {
 			opened.pop();
 			}
 			
+			for(let i = 0; i < pickCard.length; i++){
+			pickCard[i].style.pointerEvents="auto";
+			}
+			
 			for(let i = 0; i < matched.length; i++){
 			matched[i].style.pointerEvents="none";
-			}		
+			}					
 	}
 
 	 setTimeout(function(){
@@ -143,14 +154,22 @@ function rating () {
 function endGame () {
 	
 	if (matched.length === 16) { 
-	 
-	alert("Congratulations! You win with "+moves+" moves! :)");
+	
+	stopTimer();
+ 
+	alert("Congratulations! You win with "+moves+" moves and "+finalTime+" seconds ! :)");	
+	
+/*	if (confirm("Congratulations! You win with "+moves+" moves and "+finalTime+" seconds ! :) Would you like to play again?")) {
+    restartGame();
+} else {
+    alert("Thank you to play with my game!");
+}*/
 	
 	setTimeout(function(){
 	 for (let i = 0; i< pickCard.length; i++){
 	pickCard[i].setAttribute('class', 'card');
     pickCard[i].style.pointerEvents="auto";	
-	 } }, 800);
+	 } }, 1200);
 	 matched.splice(0,16);
 	 opened.splice(0,16);
 	 
@@ -168,7 +187,8 @@ function matchedDelete () {
 }
 
 function restartGame () {
-
+	
+clearTimer();
 starFirst.style.visibility='visible';
 starSecond.style.visibility='visible';
 starThird.style.visibility='visible';
@@ -183,4 +203,47 @@ moves = 0;
 movesCount.innerHTML = '<span class="moves">'+moves+'</span>';	
  
 startGame();	
+}
+
+/*TIMER*/
+
+//based on https://jsfiddle.net/Daniel_Hug/pvk6p/
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+    
+    timerDisplay.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+	
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+timer();
+
+
+function startTimer (){
+if(moves > 0) {
+timer();	
+}else clearTimer();
+}
+
+function stopTimer () {
+if (matched.length === 16) {
+    clearTimeout(t);
+	finalTime = timerDisplay.textContent;
+}
+}
+
+function clearTimer () {
+    timerDisplay.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
 }
