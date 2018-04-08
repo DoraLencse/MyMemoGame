ac6c281fc7fbcd4942b80ce50506b796894f40ba
DoraLencse <35639270+DoraLencse@users.noreply.github.com>
2018-04-08 08:51:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -19,10 +19,10 @@ let cards = ['fa fa-diamond',
 	
 let cardContent = document.querySelectorAll('li.card');
 let pickCard = document.getElementsByClassName('card');
-//let picked = [];
 let openedCards = document.getElementsByClassName('open show');
 let opened = [];
 let flipBack = document.getElementsByClassName('nomatch');
+let flipCards = [];
 let matchedCards = document.getElementsByClassName('match');
 let matched = [];
 let movesCount = document.querySelector('div.moves');
@@ -39,9 +39,6 @@ let hours = 0;
 let t;
 let modalDisplay = document.getElementById('myModal');
 
-
-restartIcon.onclick = function () {location.reload(true)};
-
 /* Shuffle function from http://stackoverflow.com/a/2450976 */
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -57,100 +54,104 @@ function shuffle(array) {
     return array;
 }
 
+restartIcon.onclick = function () {location.reload(true)};
+
 startGame();
 
 function startGame() {
 
-cards = shuffle(cards);
-for(let i = 0; i < cardContent.length; i++){
+    cards = shuffle(cards);
+    for(let i = 0; i < cardContent.length; i++){
 	cardContent[i].innerHTML = '<i class="'+cards[i]+'"></i>';
-}
+    }
 
-clickOnCards();
+    clickOnCards();
 }
  
 function clickOnCards() {
 	         
-			for(let i = 0; i < pickCard.length; i++){				
-			pickCard[i].onclick = function() {
-				if (opened.length === 2) {
-				return
-				} else
-			pickCard[i].style.pointerEvents="none";				
-		    moves++;
+    for(let i = 0; i < pickCard.length; i++){				
 			
-			startTimer();	
+    pickCard[i].onclick = function() {
+        if (opened.length === 2) {
+		return
+		} else
+		pickCard[i].style.pointerEvents="none";				
+		moves++;
 			
-			movesCount.innerHTML = '<div class="moves">'+moves+' Moves </div>';
+	    startTimer();	
 			
-			rating();
+		movesCount.innerHTML = '<div class="moves">'+moves+' Moves </div>';
 			
-			pickCard[i].setAttribute('class', 'card open show');
+		rating();
+			
+		pickCard[i].setAttribute('class', 'card open show');
 								
-			opened.push(pickCard[i]);
+		opened.push(pickCard[i]);
 			
-			setTimeout (compareCards, 1000);
+		setTimeout (compareCards, 1000);
 					
-			};	
-			}			
+		};	
+	}			
 }
 
 function compareCards () {
 	
-	if (opened.length == 2 ) {		
+	if (opened.length === 2 ) {		
 				
-			if(opened[0].innerHTML !== opened[1].innerHTML) {
-			opened[0].setAttribute('class', 'card nomatch');
-			opened[1].setAttribute('class', 'card nomatch');
-			opened.pop();
-			opened.pop();
+		if(opened[0].innerHTML !== opened[1].innerHTML) {
+		opened[0].setAttribute('class', 'card nomatch');
+		opened[1].setAttribute('class', 'card nomatch');
+		flipCards.push(opened[0]);
+		flipCards.push(opened[1]);		
+		opened.pop();
+		opened.pop();
 			
+		} else if (opened[0].innerHTML === opened[1].innerHTML){
+		opened[0].setAttribute('class', 'card match');
+		opened[1].setAttribute('class', 'card match');	
+		matched.push(opened[0]);
+		matched.push(opened[1]);			
+		opened.pop();
+		opened.pop();
+		}
 			
-			} else if (opened[0].innerHTML === opened[1].innerHTML){
-			opened[0].setAttribute('class', 'card match');
-			opened[1].setAttribute('class', 'card match');	
-			matched.push(opened[0]);
-			matched.push(opened[1]);			
-			opened.pop();
-			opened.pop();
-			}
+		for(let i = 0; i < pickCard.length; i++){
+		pickCard[i].style.pointerEvents='auto';
+		}
 			
-			for(let i = 0; i < pickCard.length; i++){
-			pickCard[i].style.pointerEvents='auto';
-			}
+		for(let i = 0; i < matched.length; i++){
+		matched[i].style.pointerEvents='none';
+		}	
 			
-			for(let i = 0; i < matched.length; i++){
-			matched[i].style.pointerEvents='none';
-			}	
-			
-			for(let i = 0; i < flipBack.length; i++){
-			flipBack[i].style.pointerEvents='none';
-			}	
+		for(let i = 0; i < flipBack.length; i++){
+		flipBack[i].style.pointerEvents='none';
+		}	
 	}
-			setTimeout (flip, 1500);
+	setTimeout (flip, 1000);
 			
-			function flip() {
-			for(let i = 0; i < flipBack.length; i++){
-			flipBack[i].style.pointerEvents='auto';
-			flipBack[i].setAttribute('class', 'card');
-			}
-}
+	function flip() {
+	    for(let i = 0; i < flipBack.length; i++){
+	    flipBack[i].style.pointerEvents='auto';
+	    flipBack[i].setAttribute('class', 'card');
+	    }
+	}
+				
 	if (matched.length === 16){		
 	setTimeout (endGame, 1000);
-	}
-	
+	}	
 }
 
 function rating () {
-	if (moves === 16 ) {
+	if (moves === 32 ) {
 	starFirst.style.visibility='hidden';
 	}
 	
-	if (moves === 32 ) {
+	if (moves === 56 ) {
 	starSecond.style.visibility='hidden';
 	}
 	
-	if (moves === 56 ) {
+	if (moves === 72 ) {
 	starThird.style.visibility='hidden';
 	}
 }
@@ -161,49 +162,49 @@ function endGame () {
 }
 
 function matchedDelete () {
- if (matched.length > 0) {
-	for (let i= 0; i < matched.length; i++) {
-	delete matched[i];	
-	}	
+    if (matched.length > 0) {
+	    for (let i= 0; i < matched.length; i++) {
+	    delete matched[i];	
+	    }	
 	matched = [];		
 	}	
 }
 
 function restartGame () {
 
-closeModal();
-moves = 0;
-movesCount.innerHTML = '<div class="moves">'+moves+' Moves </div>';	
+    closeModal();
+    moves = 0;
+    movesCount.innerHTML = '<div class="moves">'+moves+' Moves </div>';	
 
-stopTimer();	
-clearTimer();
+    stopTimer();	
+    clearTimer();
 
-starFirst.style.visibility='visible';
-starSecond.style.visibility='visible';
-starThird.style.visibility='visible';
+    starFirst.style.visibility='visible';
+    starSecond.style.visibility='visible';
+    starThird.style.visibility='visible';
 	
-for (let i = 0; i< pickCard.length; i++){
+    for (let i = 0; i< pickCard.length; i++){
 	pickCard[i].setAttribute('class', 'card');
     pickCard[i].style.pointerEvents="auto";	
-	 }
+	}
 
-matchedDelete();
+    matchedDelete();
  
-startGame();	
+    startGame();	
 }
 
 /*TIMER: based on https://jsfiddle.net/Daniel_Hug/pvk6p/*/
 
-function add() {
+    function add() {
     seconds++;
-    if (seconds >= 60) {
+        if (seconds >= 60) {
         seconds = 0;
         minutes++;
         if (minutes >= 60) {
             minutes = 0;
             hours++;
         }
-    }
+        }
     
     timerDisplay.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
 	
@@ -215,16 +216,16 @@ function timer() {
 }
 
 function startTimer (){
-if(moves === 1) {
-timer();	
-}
+    if(moves === 1) {
+    timer();	
+    }
 }
 
 function stopTimer () {
-if (matched.length === 16) {
+    if (matched.length === 16) {
     clearTimeout(t);
 	finalTime = timerDisplay.textContent;
-}
+    }
 }
 
 function clearTimer () {
@@ -234,18 +235,37 @@ function clearTimer () {
 
 /* MODAL */
 
- function winMessage() {
+function winMessage() {
  	modalDisplay.style.display = 'block';
 	let resultTime = document.querySelector('.final-time');
 	resultTime.textContent = finalTime;
 	
- let resultMove = document.querySelector('.final-move');
+	let resultMove = document.querySelector('.final-move');
 	resultMove.textContent = moves;
- }
+	
+	let finalRating = document.querySelector('.final-rating');
+	
+	
+	if (moves < 32 ) {		
+		finalRating.innerHTML = '<p class="final-rating">Your rating: <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></p>';
+        finalRating.style.cssText = 'color:#fc5b03;';		
+	}
+	
+	if (moves >= 32 && moves <= 56 ) {		
+		finalRating.innerHTML = '<p class="final-rating">Your rating: <i class="fa fa-star"></i><i class="fa fa-star"></i></p>';	
+		finalRating.style.cssText = 'color:#fc5b03;';		
+	}
+	
+	if (moves >= 56 && moves <= 72 ) {
+	finalRating.innerHTML = '<p class="final-rating">Your rating: <i class="fa fa-star"></i></p>';
+	finalRating.style.cssText = 'color:#fc5b03;';	
+	}
+	
+}
 
- function closeModal() {
+function closeModal() {
  	modalDisplay.style.display = 'none';
- }
+}
 
 document.getElementById('modalClose').addEventListener('click', closeModal);
 document.querySelector('.again').addEventListener('click',restartGame);
